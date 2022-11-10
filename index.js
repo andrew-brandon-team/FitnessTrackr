414e2eb70d9c37f79a88b5d835b540b77825c8f7
Brandon Varney <b.varney03@gmail.com>
2022-11-10 17:47:14 -0500

    adjusting index file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // This is where we will be building our Express server.
 // 1: Import express
 const express = require('express');
+const { client } = require('./db/index')
 // 2: Setting up new express server instance
 const app = express();
 
+
+client.connect()
 // 3: Set up your port to listen to our server
 app.listen(3000, () => {
   console.log("We are now running on port 3000");
