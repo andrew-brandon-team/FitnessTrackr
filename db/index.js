74555fb5676913b38fecbf188d2cb6f0674ad527
Andrew Cook <andrewjcook93@gmail.com>
2022-11-10 16:33:29 -0600

    set up express and pg client

--- db/index.js
+++ db/index.js
@@ -1,1 +1,16 @@
 // This is where we will be building our pg.Client instance
+
+// 1. Import pg
+const pg = require('pg');
+const { client } = require('./db/index');
+
+// 2. Make a new pg.client instance
+const client = new pg.Client('postgres://localhols:5432/fitness-dev')
+
+// 3. Don't forget to connect your db client
+client.connect();
+
+// Last step. Export the db client
+module.exports = {
+  client
+}
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // This is where we will be building our Express server.
+// 1: Import express
+const express = require('express');
+// 2: Setting up new express server instance
+const app = express();
+
+// 3: Set up your port to listen to our server
+app.listen(3000, () => {
+  console.log("We are now running on port 3000");
+});
+
