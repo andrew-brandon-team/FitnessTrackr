91140277067e31641a42210b5cfd60d42ce4d7be
Brandon Varney <b.varney03@gmail.com>
2022-11-12 10:23:32 -0500

    Adding info from original project

---
+++ db/users.js
@@ -0,0 +1,24 @@
+const client = require("./client");
+
+async function createUser({ username, password }) {
+
+}
+
+async function getUser ({ username, password }) {
+
+}
+
+async function getUserbyId (userId) {
+
+}
+
+async function getUserByUsername(userName) {
+
+}
+
+module.exports = {
+    createUser,
+    getUser,
+    getUserbyId,
+    getUserByUsername
+}
