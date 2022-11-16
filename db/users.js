3f1a0109ba3983e8bb851c067ab3e7c6cf0f8a64
Brandon Varney <b.varney03@gmail.com>
2022-11-16 13:51:00 -0500

    hashing password

--- db/users.js
+++ db/users.js
@@ -1,13 +1,16 @@
 const client = require("./client");
+const bcrypt = require('bcrypt')
 
 async function createUser({ username, password }) {
+    const SALT_COUNT = 10;
+    const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
     try {
         const { rows: [user] } = await client.query(`
         INSERT INTO users(username, password)
         VALUES ($1, $2)
         ON CONFLICT (username) DO NOTHING
-        RETURNING *
-        `, [username, password]);
+        RETURNING id, username;
+        `, [username, hashedPassword]);
 
         return user
     } catch (error) {
@@ -16,14 +19,19 @@ async function createUser({ username, password }) {
 }
 
 async function getUser ({ username, password }) {
+    if (!username || !password) return
     try {
-        const { rows: [user]} = await client.query(`
-        SELECT id, username
-        from users
-        WHERE id=${ username };
-        `);
+        const user = await getUserByUsername(username)
 
-        return user
+        const hashedPassword = user.password
+        const passwordMatched = await bcrypt.compare(password, hashedPassword)
+        if (passwordMatched) {
+            delete user.password;
+            return user;
+        } else {
+            return null;
+        }
+
     } catch (error) {
         console.log(error)
     }
