b9e209b02b785ab30f309fc8fdcc984b1543a703
Brandon Varney <b.varney03@gmail.com>
2022-11-18 22:51:20 -0500

    adding activities and routine_activities

--- api/activities.js
+++ api/activities.js
@@ -9,7 +9,8 @@ const {
   getActivityByName, 
   attachActivitiesToRoutines, 
   createActivity, 
-  updateActivity
+  updateActivity,
+  getPublicRoutinesByActivity
 } = require('../db');
 const { post } = require('./users');
 
@@ -17,7 +18,6 @@ const { post } = require('./users');
 activitiesRouter.get('/', async (req, res, next) => {
   try {
     const allActivities = await getAllActivities();
-
     res.send({
       allActivities
     });
@@ -55,7 +55,6 @@ activitiesRouter.post('/', async (req, res, next) => {
 activitiesRouter.patch('/:activityId', requireUser, async (req, res, next) => {
   const {activityId} = req.params;
   const {name, description} = req.body;
-
   const updateFields = {};
 
   if (name) {
@@ -66,13 +65,37 @@ activitiesRouter.patch('/:activityId', requireUser, async (req, res, next) => {
     updateFields.description = description;
   }
 
-  // try {
-  //   const originalActivity = await getActivityById(activityId);
-
-  //   if (originalActivity)
-  // }
+  try {
+    if (req.user) {
+      const updatedActivity = await updateActivity(activityId, updateFields);
+      res.send({ activity: updatedActivity});
+    } else {
+      next ({
+        name: "NotLoggedIn",
+        message: "Please Login to Update Activity"
+      })
+    }
+  } catch (error) {
+    console.log({name, message})
+  }
 });
 
 // GET /activities/:activityId/routines
+activitiesRouter.get('/:activityId/routines', async (req, res, next) =>{
+  try {
+    const id = req.params.activityId;
+    const activity = {id: id};
+    const routines = await getPublicRoutinesByActivity(activity);
+    if (routines.length === 0)
+      res.send({
+        message: "Activity not found",
+        name: "Activity does not exist",
+        error: "Activity does not currently exist",
+      });
+    res.send(routines);
+  } catch ({name, message }) {
+    console.log({name, message})
+  }  
+})
 
-module.exports = acitivitiesRouter;
+module.exports = activitiesRouter;
