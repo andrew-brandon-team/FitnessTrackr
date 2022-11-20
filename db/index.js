26226b3e8b779eb6d0c6c6bae08992828382bc31
Andrew Cook <andrewjcook93@gmail.com>
2022-11-20 14:31:25 -0600

    index.js changes for render

--- db/index.js
+++ db/index.js
@@ -5,7 +5,18 @@ const pg = require('pg');
 const { client } = require('./db/index');
 
 // 2. Make a new pg.client instance
-const client = new pg.Client(process.env.DB_URL || 'postgres://localhost:5432/fitness-dev' )
+let client 
+if (process.env.db_url) {
+  new pg.Client(process.env.db_url)
+} else {
+  client = new pg.Client({
+    host: 'localhost',
+    database: 'fitness-dev',
+    port: 5432,
+    user: 'sleazycook',
+    password: ''
+  })
+}
 
 // 3. Don't forget to connect your db client
 client.connect();
